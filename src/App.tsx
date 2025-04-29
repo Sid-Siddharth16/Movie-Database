fa2a30e16a22a86ef6ecc37f6d12218408dfc36d
Siddharth Pandey <siddharthpandeyhoneypandey@gmail.com>
2025-04-29 11:04:33 +0530

    route-update

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,18 @@ import Navbar from './Layout/Navbar'
 import {Routes, Route } from 'react-router'
 
 function App() {
+
+
   
 
   return (
-    <>
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+      </Routes>
       {/* <Login/> */}
       <Navbar/>
-    </>
+    </div>
   )
 }
 
