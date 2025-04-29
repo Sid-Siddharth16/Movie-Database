d57758ec87085c94945c81b905b7114a8924de80
Anjulsinghal <anjulsinghal123@gmail.com>
2025-04-29 12:26:45 +0530

    nothing

--- src/Layout/Navbar.tsx
+++ src/Layout/Navbar.tsx
@@ -1,6 +1,6 @@
 import logo from "../assets/logo-img.jpg";
 
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 const Navbar: React.FC = () => {
   const navigate = useNavigate();
@@ -11,21 +11,21 @@ const Navbar: React.FC = () => {
     <div>
       <nav className="flex justify-between items-center bg-gray-800 text-white p-4">
         <div className="max-w-1/3 ml-10">
-          <img src={logo} alt="logo" className="max" />
+          <img src={logo} alt="logo" className="max-w-20 bg-blend-multiply" />
         </div>
         <div className="max-w-1/3 ml-10">
           <ul className="flex justify-between gap-8">
-            <li>
-              <a href="#">Home</a>
+            <li>  
+              <Link to="#">Home</Link>
             </li>
             <li>
-              <a href="#">Movies</a>
+              <Link to="#">Movies</Link>
             </li>
             <li>
-              <a href="#">Contact</a>
+              <Link to="#">Contact</Link>
             </li>
             <li>
-              <a href="#">About</a>
+              <Link to="#">About</Link>
             </li>
           </ul>
         </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,34 @@
-import React from 'react'
+// import React from 'react';
+import { RouteObject } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
 
-const RouteWrapper = () => {
-  return (
-    <div>
-        <Router>
-            <Routes>
-                Route
-            </Routes>
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: (
+      <ProtectedRoute>
+        <Home />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/abc',
+    element: (
+      <ProtectedRoute>
+        //abc
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '*',
+    element: <Navigate to="/" replace />,
+  },
+];
 
-        </Router>
-    </div>
-  )
-}
-
-export default RouteWrapper
+export default routes;
