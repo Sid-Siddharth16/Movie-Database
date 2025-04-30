ed04d51584665fe75c10ab512a90f135335c70fa
Siddharth Pandey <siddharthpandeyhoneypandey@gmail.com>
2025-04-30 13:03:38 +0530

    updates

--- src/Layout/Navbar.tsx
+++ src/Layout/Navbar.tsx
@@ -1,40 +1,51 @@
+import { useState } from "react";
 import logo from "../assets/logo-img.jpg";
 import { BiUser } from "react-icons/bi";
 import { useNavigate } from "react-router";
 
 const Navbar: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
   const navigate = useNavigate();
+
   const handleClick = () => {
     navigate("/login");
+  };
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
   };
 
   return (
     <div>
       <nav className="flex justify-between items-center bg-gray-800 text-white p-4">
-        <div className="ml-10 rounded-lg" >
+        {/* Logo */}
+        <div className="ml-10 rounded-lg">
           <img
             src={logo}
             alt="logo"
-            className="h-12 w-auto object-contain rounded-full "
+            className="h-12 w-auto object-contain rounded-full"
           />
         </div>
 
+        {/* Title */}
         <div>
-          <ul className="flex justify-between gap-8">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">Movies</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-          </ul>
+          <span className="cursor-pointer font-extrabold text-3xl">
+            MDb - Movie Database
+          </span>
         </div>
+
+        {/* Search Input */}
+        <div>
+          <input
+            type="text"
+            placeholder="Search here..."
+            value={searchQuery}
+            onChange={handleSearchChange}
+            className="px-2 py-1 rounded-md text-black"
+          />
+        </div>
+
+        {/* User/Login Buttons */}
         <div className="mr-10 flex items-center gap-2">
           <button className="rounded-full border p-1 hover:bg-gray-700">
             <BiUser size={20} />
