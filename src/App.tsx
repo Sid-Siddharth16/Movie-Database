8671e88d639a62c51c648f0e3e667712cd223863
Siddharth Pandey <siddharthpandeyhoneypandey@gmail.com>
2025-04-30 11:36:34 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import "./styles/App.css";
 import Login from "./pages/Login";
-// import Navbar from "./Layout/Navbar";
-import { Routes, Route } from "react-router";
 import SignUp from "./pages/SignUp";
-// import SlideShow from "./components/SlideShow";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./Layout/MainLayout";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </MainLayout>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import SlideShow from '../components/SlideShow'
-import Navbar from '../Layout/Navbar'
 
 const Home : React.FC= ()  => {
   return (
     <div>
-        <Navbar/>
         <SlideShow/>
     </div>
   )
